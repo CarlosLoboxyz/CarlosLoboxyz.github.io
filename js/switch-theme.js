3f09d857175d8c539824d18a84e4b93dc92b8aed
CarlosLoboxyz <CarlosLoboxyz@users.noreply.github.com>
2022-10-24 20:30:06 +0000

    deploy: 5013e53de226ca3d92e20bd384b9fc893f1b1c9c

--- js/switch-theme.js
+++ js/switch-theme.js
@@ -5,24 +5,24 @@ const currentTheme = localStorage.getItem("theme");
 
 // Function that will switch the theme based on the if the theme toggle is checked or not
 function switchTheme() {
-  if (document.documentElement.getAttribute("data-theme") === "dark") {
-    document.documentElement.setAttribute("data-theme", "light");
-    // Set the user's theme preference to dark
-    localStorage.setItem("theme", "light");
-  } else {
-    document.documentElement.setAttribute("data-theme", "dark");
-    // Set the user's theme preference to light
-    localStorage.setItem("theme", "dark");
-  }
+  	if (document.documentElement.getAttribute("data-theme") === "dark") {
+    	document.documentElement.setAttribute("data-theme", "light");
+    	// Set the user's theme preference to dark
+    	localStorage.setItem("theme", "light");
+  	} else {
+    	document.documentElement.setAttribute("data-theme", "dark");
+    	// Set the user's theme preference to light
+    	localStorage.setItem("theme", "dark");
+  	}
+	// Get all the animate elements & add an animation.
+	[...document.getElementsByClassName('animate')].forEach((element) => {
+		element.style.transition = "0.4s";
+	});
 }
 
 
 // Get the current theme from local storage
 if (currentTheme) {
   // Set the body data-theme attribute to match the local storage item
-  // If the current theme is dark, check the theme toggle
   document.documentElement.setAttribute("data-theme", currentTheme);
-  if (currentTheme === "dark") {
-    themeToggle.click();
-  }
 }
